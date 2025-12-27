70e0aac26b69551261e3504789a5c4dc44ead690
SusheniUmayangana <umayangiks@gmail.com>
2025-12-27 13:49:12 +0530

    Initial commit: added portfolio project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-hidden">
+
+            {/* Theme Toggle */}
+
+            {/* Background Effects */}
+
+            {/* Navbar */}
+
+            {/* Main Content */}
+
+            {/* Footer */}
+
+        </div>
+
+    );
+
+};
